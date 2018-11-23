796f21f3cf06ce4d7e25202babc95cd5b5b29692
janvrieler <43779566+janvrieler@users.noreply.github.com>
2018-11-23 12:44:40 +0100

    Update Data exploration.R

--- Data exploration.R
+++ Data exploration.R
@@ -1,1 +1,19 @@
 summary(digits.train)
+
+regression <- lm(V65 ~ ., data = digits.train)
+
+
+class.observations = digits.test$V65
+class.prediction = predict.lm(regression, digits.test)
+class.prediction = round(class.prediction)
+
+for (i in 1:length(class.prediction)) {
+  if (class.prediction[i] == -1) {
+    class.prediction[i] = 0
+  }
+  if(class.prediction[i] == 10) {
+    class.prediction[i] = 9
+  }
+}
+
+table(class.observations, class.prediction)
