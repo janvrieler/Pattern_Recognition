a8150edf9f3bfeb9aa334b87094845d169e1a4f1
janvrieler <43779566+janvrieler@users.noreply.github.com>
2018-12-11 14:49:08 +0100

    fgtnfg

--- Multinomial_logit_density.R
+++ Multinomial_logit_density.R
@@ -1,6 +1,6 @@
 library(nnet)
 
-mnist.multinom <- multinom(label ~ density.scaled, data = mnist, maxit = 1000)
+mnist.multinom <- multinom(label ~ ratio.scaled + density.scaled, data = mnist, maxit = 1000)
 
 mnist.multinom.pred <- predict(mnist.multinom, mnist, type = "class")
 
