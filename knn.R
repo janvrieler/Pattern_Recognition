d77e683f50faa8310fd4e39e2bd22e5a8c5b9a02
Rens van Schouwenburg <rensvanschouwenburg@gmail.com>
2018-12-14 12:21:52 +0100

    small changes report and knn
    knn simplified and pieces added to report

--- knn.R
+++ knn.R
@@ -6,9 +6,9 @@ library(e1071)
 
 set.seed(123)
 
-ind <- createDataPartition(as.factor(mnist$label), p = 1/7, list = FALSE)
-mnist.train <- mnist[ind,]
-mnist.test <- mnist[-ind,]
+ind <- createDataPartition(as.factor(cropped.mnist$label), p = 1/7, list = FALSE)
+mnist.train <- cropped.mnist[ind,]
+mnist.test <- cropped.mnist[-ind,]
 
 mnist.train$label <- as.factor(mnist.train$label)
 
@@ -19,9 +19,9 @@ test.labels <- mnist.train$label
 levels(test.labels) <- unique(test.labels)
 levels(test.labels) <- make.names(levels(test.labels))
 
-controlParameters <- trainControl(method = "cv", number = 3, savePredictions = TRUE, classProbs = TRUE, verboseIter = TRUE)
+controlParameters <- trainControl(method = "cv", number = 3, savePredictions = TRUE, classProbs = TRUE, verboseIter = FALSE)
 
 
 #tunelength = 3 to test on 4 different k-values and pick the highest accuracy
-my_knn <- train(mnist.train[,2:785], test.labels, method = "knn", data = mnist.train, trControl = controlParameters, tunelength = 3)
+my_knn <- train(mnist.train[,2:length(mnist.train)], test.labels, method = "knn", data = mnist.train, trControl = controlParameters, tunelength = 1)
 print(my_knn)
