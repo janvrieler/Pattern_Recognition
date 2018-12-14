e544aff8749137f04e26c973969d0c6f2fae482a
Rens van Schouwenburg <rensvanschouwenburg@gmail.com>
2018-12-14 10:08:44 +0100

    Update knn.R
    error on levels is probably fixed

--- knn.R
+++ knn.R
@@ -1,4 +1,8 @@
+library(lattice)
+library(ggplot2)
 library(caret)
+library(class)
+library(e1071)
 
 set.seed(123)
 
@@ -8,11 +12,16 @@ mnist.test <- mnist[-ind,]
 
 mnist.train$label <- as.factor(mnist.train$label)
 
+#make copy of the labels to test creating levels 
+#&& 
+#remove Error: At least one of the class levels is not a valid R variable name
+test.labels <- mnist.train$label
+levels(test.labels) <- unique(test.labels)
+levels(test.labels) <- make.names(levels(test.labels))
+
 controlParameters <- trainControl(method = "cv", number = 5, savePredictions = TRUE, classProbs = TRUE)
 
-library(class)
-library(e1071)
 
 #tunelength = 4 to test on 4 different k-values and pick the highest accuracy
-my_knn <- train(mnist.train[,2:785], mnist.train$label, method = "knn", data = mnist.train, trControl = controlParameters, tunelength = 4)
+my_knn <- train(mnist.train[,2:785], test.labels, method = "knn", data = mnist.train, trControl = controlParameters, tunelength = 4)
 print(my_knn)
